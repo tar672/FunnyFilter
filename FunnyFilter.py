35ffff249d7206bd296ea3918ecfe7328881850e
Tom Robertson <tomasrobertson@ymail.com>
2013-02-18 21:55:59 +0000

    before switiching to sample API

--- FunnyFilter.py
+++ FunnyFilter.py
@@ -1,3 +1,7 @@
+import time
+import threading
+import json
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -6,22 +10,68 @@ import pusher
 
 import config
 
-p = pusher.Pusher(app_id=config.app_id, key=config.app_key, secret=config.app_secret)
+from flask import Flask, request
 
-
-class StdOutListener(StreamListener):
-	""" A listener handles tweets are the received from the stream. 
-	This is a basic listener that just prints received tweets to stdout.
-	"""
+class TweetListener(StreamListener):
 	def on_data(self, data):
-		p['my-channel'].trigger('my_event',{'msg': data})
+		global terms
+		print "tweet!"
+		for t in terms:
+			if(t not in data):
+				continue
+
+			if(len(terms[t]['cache'])+len(data) < 900):
+				if(terms[t]['cache'] != ""):
+					data = "," + data
+				terms[t]['cache'] += data
+
+			if(terms[t]['time'] < time.time()-3):
+				print "sent!"
+				p['all-tweets'].trigger(t, "[" + terms[t]['cache'] + data + "]")
+
+				terms[t]['time'] = time.time()
+				terms[t]['cache'] = ""
+
 		return True
+
 	def on_error(self, status):
 		print status
 
+new = True
+def getStream():
+	global new
+	stream = Stream(auth, l)
+	while(1):
+		if(new):
+			print "update stream"
+			stream.disconnect()
+			t = threading.Thread(target=stream.filter, kwargs={'track':terms})
+			t.daemon = True
+			t.start()
+			new = False
+		time.sleep(2)
+
+p = pusher.Pusher(app_id=config.app_id, key=config.app_key, secret=config.app_secret)
+terms = {"maithu":{"time":0, "cache":""}}
+
+l = TweetListener()
+auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+
+t = threading.Thread(target=getStream)
+t.daemon = True
+t.start()
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/addTerm')
+def addTerm():
+	global new, terms
+	print "addTerm: " + request.args.get("term")
+	terms[request.args.get("term")] = {"time":0, "cache":""}
+	new = True
+	return "yay"
+	
 if __name__ == '__main__':
-	l = StdOutListener()
-	auth = OAuthHandler(config.consumer_key, config.consumer_secret)
-	auth.set_access_token(config.access_token, config.access_token_secret)
-	stream = Stream(auth, l)	
-	stream.filter(track=['basketball'])
+	app.run(host='0.0.0.0', port=8000, use_reloader=False)
